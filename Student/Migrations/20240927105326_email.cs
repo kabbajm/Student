da7c1299b494d58b84e786cbd210ae90850371e7
MYLAHBIB <youssefmylahbib@gmail.com>
2024-09-27 11:54:12 +0100

    add column email

---
+++ Student/Migrations/20240927105326_email.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Student.Migrations
+{
+    /// <inheritdoc />
+    public partial class email : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "mail",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "mail",
+                table: "Students");
+        }
+    }
+}
--- Student/Models/Students.cs
+++ Student/Models/Students.cs
@@ -5,5 +5,6 @@
         public int id { get; set; }
         public string name { get; set; }
         public int age { get; set; }
+        public string mail { get; set; }
     }
 }
