5e9a588b0ae0dd51fe057a28c37d1a93a2728a06
Mohammed Issam KABBAJ <kabbaj@emi.ac.ma>
2024-09-27 12:24:03 +0100

    testnew

--- Student/Migrations/20240927112314_couco.Designer.cs
+++ Student/Migrations/20240927112314_couco.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Student.Data;
 
@@ -10,9 +11,11 @@ using Student.Data;
 namespace Student.Migrations
 {
     [DbContext(typeof(StudentContext))]
-    partial class StudentContextModelSnapshot : ModelSnapshot
+    [Migration("20240927112314_couco")]
+    partial class couco
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
